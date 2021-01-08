eeb33dc30bf51bf1bce954b8e89f6f188b0374aa
Diego Sevilla <sevilla.diego8@gmail.com>
2021-01-07 22:53:10 -0600

    feat: :art: Hobbies responsive final adjustments

--- src/components/Hobbies/Hobbies.js
+++ src/components/Hobbies/Hobbies.js
@@ -16,56 +16,57 @@ import crowd from '../../images/crowd2.png';
 const hobbies = () =>{
     return(
         <Aux>
-        <section className={classes.sectionHobbies}>
-            <div id="hobbies" className={classes.Hobbies}>
-                <div className={classes.innerDivHobbies}>
-                    <h3 className={classes.titleHobbies}>Hobbies</h3>
-                    <Row className='row rowMarginHobbies'>
-                        <Col xs="2" >
-                        </Col>
-                        <Col xs="8" >
-                            <div >
-                                <p className={classes.pHobbiesDescription}>I like to play the piano! I think music is a cool 
-                                way to express yourself to others. Though, it helps you remind good thinks and moments of life.
-                                </p>
-                            </div>
-                        </Col>
-                        <Col xs="2" >
-                        </Col>
-                    </Row>
-                    <Row >
-                        <Col >
-                            <Piano/>
-                        </Col>
-                    </Row>
+            <section className={classes.sectionHobbies}>
+                <div id="hobbies" className={classes.Hobbies}>
+                    <div className={classes.innerDivHobbies}>
+                        <h3 className={classes.titleHobbies}>Hobbies</h3>
+                        <Row className='row'>
+                            <Col xs="2" >
+                            </Col>
+                            <Col xs="8" >
+                                <div >
+                                    <p className={classes.pHobbiesDescription}>
+                                        I like to play the piano! I think music is a cool way to express yourself to others. 
+                                        It helps you remind good things and moments of life.
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col xs="2" >
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col >
+                                <Piano/>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section className={classes.sectionHobbies}>
-            <div id="hobbies" className={classes.Hobbies}>
-                <div className={classes.innerDivHobbies}>
-                <Row className='row rowMarginHobbies'>
-                        <Col xs="2" >
-                        </Col>
-                        <Col xs="8" >
-                            <div >
-                                <p className={classes.pHobbiesDescriptionVar}>
-                                    I have climbed some volcanoes! I see it like a way of escape of all human being wars. 
-                                    At the top, you can stop time and reflect on how insignificant we are.
-                                </p>
-                            </div>
-                        </Col>
-                        <Col xs="2" >
-                        </Col>
-                    </Row>
-                    <Row >
-                        <Col >
-                        </Col>
-                    </Row>
+            </section>
+            <section className={classes.sectionHobbies}>
+                <div id="hobbies" className={classes.Hobbies}>
+                    <div className={classes.innerDivHobbies}>
+                    <Row className='row'>
+                            <Col xs="2" >
+                            </Col>
+                            <Col xs="8" >
+                                <div >
+                                    <p className={classes.pHobbiesDescriptionVar}>
+                                        I have climbed some volcanoes! I see it like a way of escape of all human being wars. 
+                                        At the top, you can stop time and reflect on how insignificant we are.
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col xs="2" >
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col >
+                            </Col>
+                        </Row>
+                    </div>
+                    <Mountains/>
                 </div>
-                <Mountains/>
-            </div>
-        </section>
+            </section>
         </Aux>
     )
 }
--- src/components/Hobbies/Mountains/Mountains.js
+++ src/components/Hobbies/Mountains/Mountains.js
@@ -6,27 +6,34 @@ const mountains = () =>{
         <div class="bg">
             <div class="mountain">
                 <div class="mountain-top">
-                <div class="mountain-cap-1"></div>
-                <div class="mountain-cap-2"></div>
-                <div class="mountain-cap-3"></div>
+                    <div class="mountain-cap-1"></div>
+                    <div class="mountain-cap-2"></div>
+                    <div class="mountain-cap-3"></div>
                 </div>
             </div>
             <div class="mountain-two">
                 <div class="mountain-top">
-                <div class="mountain-cap-1"></div>
-                <div class="mountain-cap-2"></div>
-                <div class="mountain-cap-3"></div>
+                    <div class="mountain-cap-1"></div>
+                    <div class="mountain-cap-2"></div>
+                    <div class="mountain-cap-3"></div>
                 </div>
             </div>
             <div class="mountain-three">
                 <div class="mountain-top">
-                <div class="mountain-cap-1"></div>
-                <div class="mountain-cap-2"></div>
-                <div class="mountain-cap-3"></div>
+                    <div class="mountain-cap-1"></div>
+                    <div class="mountain-cap-2"></div>
+                    <div class="mountain-cap-3"></div>
                 </div>
             </div>
-            <div class="cloud"></div>
-            <div class="cloud2"></div>
+            <div class="mountain-four">
+                <div class="mountain-top-4">
+                    <div class="mountain-cap-1"></div>
+                    <div class="mountain-cap-2"></div>
+                    <div class="mountain-cap-3"></div>
+                </div>
+            </div>
+            <div class="cloudd"></div>
+            <div class="cloudd2"></div>
             <div class="div-mountain-title">
                 <p class="mountain-title" >I have climbed some volcanoes! I see it like a way of escape of all human being wars. 
                     At the top, you can stop time and reflect on how insignificant we are.</p>
