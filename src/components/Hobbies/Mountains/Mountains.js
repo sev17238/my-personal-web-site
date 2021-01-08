68aa99a5d2c43631d97fc1d94f085385bfeccb3e
Diego Sevilla <sevilla.diego8@gmail.com>
2021-01-07 20:36:33 -0600

    feat: :art: Add Hobbies sections
    Audios are not being used yet

---
+++ src/components/Hobbies/Mountains/Mountains.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Mountains.css';
+
+const mountains = () =>{
+    return(
+        <div class="bg">
+            <div class="mountain">
+                <div class="mountain-top">
+                <div class="mountain-cap-1"></div>
+                <div class="mountain-cap-2"></div>
+                <div class="mountain-cap-3"></div>
+                </div>
+            </div>
+            <div class="mountain-two">
+                <div class="mountain-top">
+                <div class="mountain-cap-1"></div>
+                <div class="mountain-cap-2"></div>
+                <div class="mountain-cap-3"></div>
+                </div>
+            </div>
+            <div class="mountain-three">
+                <div class="mountain-top">
+                <div class="mountain-cap-1"></div>
+                <div class="mountain-cap-2"></div>
+                <div class="mountain-cap-3"></div>
+                </div>
+            </div>
+            <div class="cloud"></div>
+            <div class="cloud2"></div>
+            <div class="div-mountain-title">
+                <p class="mountain-title" >I have climbed some volcanoes! I see it like a way of escape of all human being wars. 
+                    At the top, you can stop time and reflect on how insignificant we are.</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default mountains;
---
+++ src/components/Hobbies/Piano/Piano.js
@@ -0,0 +1,58 @@
+import React,{Component} from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import './Piano.css';
+
+//notes
+import C from '../../../notes/C.mp3'
+import Db from '../../../notes/Db.mp3'
+import D from '../../../notes/D.mp3'
+import Eb from '../../../notes/Eb.mp3'
+import E from '../../../notes/E.mp3'
+import F from '../../../notes/F.mp3'
+import Gb from '../../../notes/Gb.mp3'
+import G from '../../../notes/G.mp3'
+import Ab from '../../../notes/Ab.mp3'
+import A from '../../../notes/A.mp3'
+import Bb from '../../../notes/Bb.mp3'
+import B from '../../../notes/B.mp3'
+
+class Piano extends Component{
+    render(){
+
+        return(
+            <Aux>
+                <div className='upperPiano'>
+                    <div className="piano">
+                        <div data-note="C" class="key white"></div>
+                        <div data-note="Db" class="key black"></div>
+                        <div data-note="D" class="key white"></div>
+                        <div data-note="Eb" class="key black"></div>
+                        <div data-note="E" class="key white"></div>
+                        <div data-note="F" class="key white"></div>
+                        <div data-note="Gb" class="key black"></div>
+                        <div data-note="G" class="key white"></div>
+                        <div data-note="Ab" class="key black"></div>
+                        <div data-note="A" class="key white"></div>
+                        <div data-note="Bb" class="key black"></div>
+                        <div data-note="B" class="key white"></div>
+                    </div>
+                </div>
+
+                <audio id="C"  src={C} ></audio>
+                <audio id="Db" src={Db}></audio>
+                <audio id="D"  src={D} ></audio>
+                <audio id="Eb" src={Eb}></audio>
+                <audio id="E"  src={E} ></audio>
+                <audio id="F"  src={F} ></audio>
+                <audio id="Gb" src={Gb}></audio>
+                <audio id="G"  src={G} ></audio>
+                <audio id="Ab" src={A} ></audio>
+                <audio id="A"  src={Ab}></audio>
+                <audio id="Bb" src={Bb}></audio>
+                <audio id="B"  src={B} ></audio>
+            </Aux>
+        )
+    }
+}
+
+export default Piano;
